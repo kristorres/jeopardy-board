1718c892b480137f73074298988e1c8a92b7ac6d
Kris Torres <kristorresdayo@gmail.com>
2021-05-14 14:53:38 -0700

    Fix scrolling in the ChampionView

--- JeopardyBoard/Views/ChampionView.swift
+++ JeopardyBoard/Views/ChampionView.swift
@@ -37,41 +37,51 @@ struct ChampionView: View {
         else {
             ZStack {
                 Image("trophy").resizable().scaledToFit()
-                VStack(spacing: 160) {
+                VStack {
                     HStack(alignment: .top) {
                         Text("CHAMPION").font(.custom("PT Sans Bold", size: 64))
                         Spacer()
                         startANewGameButton
                     }
-                    ScrollView(.vertical) {
-                        ForEach(champions) { champion in
-                            VStack {
-                                Text(champion.name.uppercased())
-                                    .font(.custom("PT Sans Bold", size: 120))
-                                Text("\(champion.score)")
-                                    .font(.custom("PT Sans Bold", size: 64))
+                    if champions.count == 1 {
+                        Spacer()
+                        championRow(for: champions.first!)
+                        Spacer()
+                    }
+                    else {
+                        ScrollView(.vertical) {
+                            ForEach(champions) {
+                                championRow(for: $0)
                             }
-                                .padding(.vertical, 64)
                         }
                     }
-                        .foregroundColor(championTextColor)
-                        .shadow(radius: 8)
-                    Spacer(minLength: 0)
                 }
             }
                 .padding(48)
         }
     }
     
-    /// The champion text color.
-    private var championTextColor: Color {
-        colorScheme == .dark ? .white : .black
-    }
-    
     /// The *Start a New Game* button, which ends the current game.
     private var startANewGameButton: some View {
         Button("START A NEW GAME", action: endGame)
             .buttonStyle(TrebekButtonStyle())
+    }
+    
+    /// Creates a row for the specified champion.
+    ///
+    /// - Parameter player: The champion to be displayed.
+    ///
+    /// - Returns: The champion row.
+    private func championRow(for player: Player) -> some View {
+        return VStack {
+            Text(player.name.uppercased())
+                .font(.custom("PT Sans Bold", size: 120))
+            Text("\(player.score)")
+                .font(.custom("PT Sans Bold", size: 64))
+        }
+            .foregroundColor((colorScheme == .dark) ? .white : .black)
+            .shadow(radius: 8)
+            .padding(.vertical, 48)
     }
     
     /// Ends the current game.
